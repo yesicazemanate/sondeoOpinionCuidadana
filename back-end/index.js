234af58cb6f6df225dd068a834b91e8bd25d4515
[brayan23] <[bdelgadomunoz@gmail.com]>
2024-07-16 10:04:04 -0500

    avance

--- back-end/index.js
+++ back-end/index.js
@@ -2,15 +2,19 @@ import express from "express"
 import cors from 'cors'
 import connectionBd from './config/db.js'
 import routes from './routes/index.js'
-import path from 'path'
+import path, { dirname } from 'path'
+import url, { fileURLToPath } from 'url'
 const PORT= 4003
 const app = express();
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
-const imagenesPathn= path.join(__dirname, "imagenes")
-console.log(imagenesPathn)
+const __filename = fileURLToPath(import.meta.url)
+console.log(__filename)
+const __dirname = dirname(__filename)
+console.log(__dirname)
+//const imagenesPathn= path.join(__dirname, "imagenes")
+//console.log(imagenesPathn)
 app.use(express.json())
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'http://localhost:4003'],
     methods: ['GET', 'POST'], 
     credentials: true 
 };
@@ -18,7 +22,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 connectionBd()
 app.use('/', routes)
-app.use('/imagenes', express.static(imagenesPathn))
+app.use('/imagenes', express.static(path.join(__dirname,'/imagenes')))
 app.listen(PORT, ()=>{
     console.log("server working PORT 4003")
 })
