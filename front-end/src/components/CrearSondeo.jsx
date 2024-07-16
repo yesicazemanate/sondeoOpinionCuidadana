0ff36beefd8dfe51aede9fd946a9fc296dd065ee
[brayan23] <[bdelgadomunoz@gmail.com]>
2024-07-15 21:38:17 -0500

    post sondeo

--- front-end/src/components/CrearSondeo.jsx
+++ front-end/src/components/CrearSondeo.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import React, { useState } from 'react'
-
+import Swal from 'sweetalert2'
 export const CrearSondeo = () => {
     const [titulo, setTitulo] = useState()
     const [descripcion, setDescripcion] = useState()
@@ -9,23 +9,34 @@ export const CrearSondeo = () => {
     const [perfilPoblacional, setPerfilPoblacional] = useState()
     const [imagen, setImagen] = useState()
     const [data, setData] = useState()
-    const handleSubmi = async(e)=>{
+    const handleSubmit = async(e)=>{
         e.preventDefault()
-        const crearSondeo = await axios.post('',{
-           titulo,descripcion,fechaApertura,fechaCierre,perfilPoblacional 
-        })
-
-        setData(crearSondeo.data)
-    }
-    const handleSumit=async(e)=>{
-        e.preventDefault();
         const imageData= new FormData()
-        imageData.append('image', imagen)
-        const response = await axios.post('http://localhost:4003/login',{
-            imgSondeo: imageData
+        imageData.append('titulo', titulo)
+        imageData.append('descripcion', descripcion)
+        imageData.append('fechaApertura', fechaApertura)
+        imageData.append('fechaCierre', fechaCierre)
+        imageData.append('perfilPoblaional', perfilPoblacional)
+        imageData.append('imgSondeo', imagen)
+        const crearSondeo = await axios.post('http://localhost:4003/sondeos', imageData,{
+            headers: {
+                'Content-Type': 'multipart/form-data',
+              },  
         })
-        
 
+       if(crearSondeo.data.msg){
+        Swal.fire({
+            title: crearSondeo.data.msg,
+            icon: "success"
+          });
+       }
+
+       setTitulo('')
+       setDescripcion('')
+       setFechaApertura('')
+       setFechaCierre('')
+       setPerfilPoblacional('')
+       setImagen(null)
     }
   return (
 <div class="bg-gray-100 flex items-center justify-center h-screen">
@@ -35,23 +46,27 @@ export const CrearSondeo = () => {
         </div>
         <h2 class="text-2xl font-semibold text-center mb-4">Crea un nuevo sondeo</h2>
         <p class="text-gray-600 text-center mb-6">Registra el nuevo sondeo</p>
-        <form className='flex gap-5' onSubmit={handleSumit}>
+        <form className='' onSubmit={handleSubmit}>
+            <div className='flex gap-5'>
             <div className=''> 
             <div class="mb-4">
                 <label for="fullName" class="block text-gray-700 text-sm font-semibold mb-2">Titulo</label>
-                <input 
+                <input
+                value={titulo} 
                 onChange={e=> setTitulo(e.target.value)}
                 type="text" id="fullName" class="form-input w-full px-4 py-2 border rounded-lg text-gray-700 focus:ring-blue-500" required placeholder="Salud,Dieta Ejercicio"/>
             </div>
             <div class="mb-4">
                 <label for="fullName" class="block text-gray-700 text-sm font-semibold mb-2">Descripcion</label>
                 <input
+                value={descripcion}
                 onChange={e=> setDescripcion(e.target.value)}
                 type="text" id="fullName" class="form-input w-full px-4 py-2 border rounded-lg text-gray-700 focus:ring-blue-500" required placeholder="Sondeo sobre la dieta"/>
             </div>
             <div class="mb-4">
                 <label for="fullName" class="block text-gray-700 text-sm font-semibold mb-2">Fecha Apertura</label>
                 <input
+                value={fechaApertura}
                 onChange={e=> setFechaApertura(e.target.value)}
                 type="date" id="fullName" class="form-input w-full px-4 py-2 border rounded-lg text-gray-700 focus:ring-blue-500" required placeholder="James Brown"/>
             </div>
@@ -60,23 +75,26 @@ export const CrearSondeo = () => {
             <div class="mb-4">
                 <label for="email" class="block text-gray-700 text-sm font-semibold mb-2">Fecha Cierre</label>
                 <input
+                value={fechaCierre}
                 onChange={e=> setFechaCierre(e.target.value)}
                 type="date" id="email" class="form-input w-full px-4 py-2 border rounded-lg text-gray-700 focus:ring-blue-500" required placeholder="hello@alignui.com"/>
             </div>
             <div class="mb-6">
                 <label for="password" class="block text-gray-700 text-sm font-semibold mb-2">Perfil Poblacional</label>
                 <input 
+                value={perfilPoblacional}
                 onChange={e=>setPerfilPoblacional(e.target.value)}
                 type="text" id="password" class="form-input w-full px-4 py-2 border rounded-lg text-gray-700 focus:ring-blue-500" required placeholder="Hombre, Mujer, mayor 14 años, etc..."/>
             </div>
             <div class="mb-4">
                 <label for="fullName" class="block text-gray-700 text-sm font-semibold mb-2">Icono del sondeo</label>
-                <input type="file"  value={imagen}
-                      onChange={(e) => setImagen(e.target.value)}required/>
+                <input type="file" 
+                      onChange={(e) => setImagen(e.target.files[0])}/>
             </div>
             </div>
+            </div>
+            <button type="submit" class=" w-full bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">Crear</button>
         </form>
-            <button type="submit" class="w-full bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">Crear</button>
     </div>
 </div>
   )
