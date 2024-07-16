72369659b3514cf1921ddf273514ac28c6b64b18
[brayan23] <[bdelgadomunoz@gmail.com]>
2024-07-15 21:51:04 -0500

    agregue rutas

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -3,7 +3,12 @@ import express from 'express'
 import routeUsuario from './routesUsuario.js'
 import routeLogin from './login.js'
 import routeSondeo from './routerSondeo.js'
+import routePregunta from './pregunta.js'
+import routeRespuesta from './respuesta.js'
+import routeParticipante from './participante.js'
+import routeCertificado from './cerficado.js'
 import path from 'path'
+import router from './routesRol.js'
 
 
 
@@ -13,5 +18,10 @@ const __dirname = path.dirname(new URL(import.meta.url).pathname);
 routes.use('/rol', routeRol)
 routes.use('/usuario', routeUsuario)
 routes.use('/login', routeLogin)
+router.use('/pregunta', routePregunta)
+router.use('/respuestausuario', routeRespuesta)
+router.use('/participante', routeParticipante)
+router.use('/certificados', routeCertificado)
+
 routes.use('/sondeos',express.static(path.join(__dirname,'imagenes')), routeSondeo)
  export default routes
