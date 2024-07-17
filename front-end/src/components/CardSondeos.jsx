5e797014136c070e0696e484d78092e28c7f6718
[brayan23] <[bdelgadomunoz@gmail.com]>
2024-07-16 22:04:13 -0500

    arreglo sondeo card

--- front-end/src/components/CardSondeos.jsx
+++ front-end/src/components/CardSondeos.jsx
@@ -4,15 +4,12 @@ import { Link } from 'react-router-dom';
 
 export const CardSondeos = () => {
     const [data, setData] = useState([]);
-    const[image, setImage]=useState('')
 
     useEffect(() => {
         const obtenerSondeos = async () => {
             try {
                 const response = await axios.get('http://localhost:4003/sondeos');
                 setData(response.data);
-
-              // setImage(response.data[0].imgSondeo)
             } catch (error) {
                 console.error('Error al obtener sondeos:', error);
             }
@@ -23,8 +20,8 @@ export const CardSondeos = () => {
     return (
         <div className='flex flex-wrap gap-4'>
             {data.map((sondeo) => (
-                <div key={sondeo._id} className='border-black border-2 w-max pl-7 pr-7 pb-2 text-center rounded-xl'>
-                    <img src={image} alt="" className='w-32' />
+                <div key={sondeo._id} className= 'flex flex-col items-center justify-center border-black border-2 w-max pl-7 pr-7 pb-2 text-center rounded-xl'>
+                    <img src={`http://localhost:4003/imagenes/${sondeo.imgSondeo}`} alt="" className='w-32 h-28 mt-5' />
                     <h1 className='font-bold mt-3'>{sondeo.titulo}</h1>
                     <div className='flex gap-5 mt-5 font-medium'>
                         <h1>Fecha Apertura: {new Date(sondeo.fechaApertura).toLocaleDateString()}</h1>
